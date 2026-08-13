4a61563c053233cad7e8472a17b8bf386cd08337
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-08-13 15:33:50 +0200

    Initiate empty project

---
+++ src/discovery/lunarpc_discovery.mli
@@ -0,0 +1,7 @@
+(*_********************************************************************************)
+(*_  lunarpc - An opinionated, lightweight, HTTP/JSON RPC harness                 *)
+(*_  SPDX-FileCopyrightText: 2024-2026 Mathieu Barbin <mathieu.barbin@gmail.com>  *)
+(*_  SPDX-License-Identifier: MIT                                                 *)
+(*_********************************************************************************)
+
+module Rpc_discovery = Rpc_discovery
---
+++ test/keyval/src/cli/keyval_cli.ml
@@ -0,0 +1,5 @@
+(*********************************************************************************)
+(*  lunarpc - An opinionated, lightweight, HTTP/JSON RPC harness                 *)
+(*  SPDX-FileCopyrightText: 2024-2026 Mathieu Barbin <mathieu.barbin@gmail.com>  *)
+(*  SPDX-License-Identifier: MIT                                                 *)
+(*********************************************************************************)
